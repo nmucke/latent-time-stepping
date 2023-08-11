74161e1292ec2f2a71d5be11aa0c5fd42ad6dddb
Nikolaj Mucke <nikolaj@spatiali.se>
2023-08-11 17:14:51 +0200

    changes

--- main_scripts/autoencoder/main_train_many_AE.py
+++ main_scripts/autoencoder/main_train_many_AE.py
@@ -29,19 +29,45 @@ torch.set_default_dtype(torch.float32)
 
 @ray.remote(num_cpus=1)
 def train_remote(
+    latent_dim,
     train_dataloader,
     val_dataloader,
     train_stepper,
-    print_progress,
     config,
 ):
+    config['model_args']['encoder']['latent_dim'] = latent_dim
+    config['model_args']['decoder']['latent_dim'] = latent_dim
 
+    oracle_model_save_path = f'{PHASE}_phase/autoencoders/WAE_{latent_dim}'
+
+    encoder = Encoder(**config['model_args']['encoder'])
+    decoder = Decoder(**config['model_args']['decoder'])
+
+    model = Autoencoder(
+        encoder=encoder,
+        decoder=decoder,
+    )
+    model = model.to(DEVICE)
+
+    optimizer = Optimizer(
+        model=model,
+        args=config['optimizer_args'],
+    )
+
+    train_stepper = WAETrainStepper(
+        model=model,
+        optimizer=optimizer,
+        model_save_path=MODEL_SAVE_PATH,
+        oracle_path=oracle_model_save_path,
+        **config['train_stepper_args'],
+    )
+    
     train(
         train_dataloader=train_dataloader,
         val_dataloader=val_dataloader,
         train_stepper=train_stepper,
-        print_progress=print_progress,
-        **config,
+        print_progress=True,
+        **config['train_args'],
     )
 
     return 0
@@ -126,73 +152,15 @@ def main():
         **config['dataloader_args'],
     )
 
-    dataloader = torch.utils.data.DataLoader(
-        dataset,
-        **config['dataloader_args'],
-    )
-
     for latent_dim in [4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48]:
 
-        config['model_args']['encoder']['latent_dim'] = latent_dim
-        config['model_args']['decoder']['latent_dim'] = latent_dim
-
-        oracle_model_save_path = f'{PHASE}_phase/autoencoders/WAE_{latent_dim}'
-
-        if MODEL_TYPE == "VAE":
-            encoder = VAEEncoder(**config['model_args']['encoder'])
-        elif MODEL_TYPE == "WAE":
-            encoder = Encoder(**config['model_args']['encoder'])
-        elif MODEL_TYPE == "AE":
-            encoder = Encoder(**config['model_args']['encoder'])
-
-        decoder = Decoder(**config['model_args']['decoder'])
-
-        model = Autoencoder(
-            encoder=encoder,
-            decoder=decoder,
-        )
-        model = model.to(DEVICE)
-
-        optimizer = Optimizer(
-            model=model,
-            args=config['optimizer_args'],
-        )
-
-        if CONTIUE_TRAINING:
-            state_dict = torch.load(f'{MODEL_SAVE_PATH}/model.pt', map_location=DEVICE)
-            model.load_state_dict(state_dict['model_state_dict'])
-            optimizer.load_state_dict(state_dict['optimizer_state_dict'])
-        
-        if MODEL_TYPE == "VAE":
-            train_stepper = VAETrainStepper(
-                model=model,
-                optimizer=optimizer,
-                model_save_path=MODEL_SAVE_PATH,
-                **config['train_stepper_args'],
-            )
-        elif MODEL_TYPE == "WAE":
-            train_stepper = WAETrainStepper(
-                model=model,
-                optimizer=optimizer,
-                model_save_path=MODEL_SAVE_PATH,
-                oracle_path=oracle_model_save_path,
-                **config['train_stepper_args'],
-            )
-        elif MODEL_TYPE == "AE":
-            train_stepper = AETrainStepper(
-                model=model,
-                optimizer=optimizer,
-                model_save_path=MODEL_SAVE_PATH,
-                **config['train_stepper_args'],
-            )
-        
         _ = train_remote.remote(
+            latent_dim=latent_dim,
             train_dataloader=train_dataloader,
             val_dataloader=val_dataloader,
-            train_stepper=train_stepper,
-            print_progress=True,
-            config=config['train_args'],
-        )
+            train_stepper=None,
+            config=config,
+        )       
 
         ray.get(_)
 
